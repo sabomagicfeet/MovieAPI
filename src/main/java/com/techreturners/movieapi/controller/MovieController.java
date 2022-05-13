ee569ac02c24ac2bd3977b7908fa6aa1ddf7d26c
aparnak22 <aparna.w@gmail.com>
2022-05-13 21:22:58 +0100

    Spring boot application and dependencies. Co-authored-by:deepatesting Co-authored-by:sabomagicfeet

---
+++ src/main/java/com/techreturners/movieapi/controller/MovieController.java
@@ -0,0 +1,2 @@
+package com.techreturners.movieapi.controller;public class MovieController {
+}
---
+++ src/test/java/com/techreturners/movieapi/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techreturners.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
