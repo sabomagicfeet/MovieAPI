5fb2c58e65d6814b354499284747e4c0b870744b
aparnak22 <aparna.w@gmail.com>
2022-05-14 00:36:58 +0100

    Adding acceptance criteria

--- src/main/java/com/techreturners/movieapi/controller/MovieController.java
+++ src/main/java/com/techreturners/movieapi/controller/MovieController.java
@@ -1,2 +1,13 @@
-package com.techreturners.movieapi.controller;public class MovieController {
+package com.techreturners.movieapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+
+    @GetMapping(value="/")
+    public String home(){
+        return "Welcome to late night coding session !";
+    }
 }
