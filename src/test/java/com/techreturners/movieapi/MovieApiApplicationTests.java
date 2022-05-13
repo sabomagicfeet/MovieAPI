28dfb2d2ae79d72c73c1b7e7f991456b081118f2
aparnak22 <aparna.w@gmail.com>
2022-05-14 00:43:12 +0100

    Adding acceptance criteria

--- src/test/java/com/techreturners/movieapi/MovieApiApplicationTests.java
+++ src/test/java/com/techreturners/movieapi/MovieApiApplicationTests.java
@@ -1,13 +1,30 @@
 package com.techreturners.movieapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class MovieApiApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvcController;
+
 	@Test
-	void contextLoads() {
+	public void testGetHome() throws Exception{
+		String expectedContent = "Welcome to late night coding session !";
+
+		this.mockMvcController.perform(
+						MockMvcRequestBuilders.get("/"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string(expectedContent));
+
 	}
 
 }
